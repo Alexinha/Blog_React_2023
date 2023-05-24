465722927f4de0a9e250e9719650b16c4c920840
Alexinha Chang <alychang0216@gmail.com>
2023-05-24 18:41:30 -0400

    6th commit: added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // destruct the react-router-dom package
 
 function App() {
   
@@ -8,13 +10,26 @@ function App() {
 
   /* the return values */
   return (
-    // class is a js reserved word, so in JSX use className instead
-    <div className="App"> 
-    <Navbar></Navbar>
-      <div className="content">
-        <Home></Home>
+    // surround the component with <Router>
+    <Router>
+      {/* // class is a js reserved word, so in JSX use className instead */}
+      <div className="App"> 
+      {/* navbar is always shown regardless which page it is */}
+      <Navbar></Navbar>
+        <div className="content">
+          <Switch>
+            {/* route for the homepage component */}
+            <Route exact path="/">
+              {/* when visit homepage, show home component aka blogs  */}
+              <Home></Home>
+            </Route>
+            <Route path="/create">
+              <Create></Create>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,19 @@
 // sfc + tab create functional components
+import { Link } from 'react-router-dom'; // let react to handle the web page link requests in the browser
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Alexinha's Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: '#f1356d',
                     borderRadius: '8px'
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
-
     );
 }
 
