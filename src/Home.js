0f6ff9705e48aa8643f767bc8345a2689a6128f2
Alexinha Chang <alychang0216@gmail.com>
2023-05-24 16:11:29 -0400

    4th commit: added pending notification

--- src/Home.js
+++ src/Home.js
@@ -7,20 +7,24 @@ const Home = () => {
     // hook - useState makes reactive value and change it dynamically 
     // this hook returns 2 values (variable and a function to change values), we use an array to grab them 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true); // by default set to true, only when data is successfully fetched set to false
 
     // runs every render, [ ] dependency array as the 2nd argument, when it's empty it only runs at first render
     // fetch data from json server
+    // to initialize json server: npx json-server --watch data/db.json --port 8000
     useEffect(() => {
         fetch('http://localhost:8000/blogs').then(res => { // get the response object first
             return res.json(); // use json() to pass json into jabascript object for us
         })
         .then((data)=>{
             setBlogs(data);
+            setIsPending(false);
         });
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div>is loading...</div>}
             {/* use props to pass the parent's data (blog) into the child (bloglist) */}
             {/* use blogs && to evaluate if blogs are still null. only til the blogs received the data from json they are outputted to the screen.  */}
             {blogs && <Bloglist blogs={blogs} title="All blogs!"></Bloglist>}
